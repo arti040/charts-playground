112b57916b8a242d2e3d9b4cc6ec843be9cf89ba
ppotera <ja@piotrpotera.com>
2017-12-05 12:21:41 +0100

    Next one serie added to the plot.

--- app/app.helpers.ts
+++ app/app.helpers.ts
@@ -5,7 +5,7 @@ export function parseChartData(rdata) {
 
 	let valShr = { color: '#06c', data: [], name: 'Val Shr' }	
 	let trend = { color: '#036', data: [], name: 'Trend' }
-	let localOutliner = { color: '#ffa500', data: [], name: 'Local Outliner' }
+	let localOutliner = { color: '#ffa500', data: [], type: 'scatter', dataLabels: { enabled: true, format: '{ point.y:.2f }' }, name: 'Local Outliner' }
 	let globalOutliner = { color: '#f00', data: [], name: 'Global Outliner' }
 	let range = { color: '#9DC3E3', data: [], name: 'Range' }
 	
@@ -20,7 +20,7 @@ export function parseChartData(rdata) {
 		
 		chartData.series[0].data.push(item.TARGET_VAR);
 		chartData.series[1].data.push(countTrend(item.INTERCEPT, item.SLOPE));
-		//chartData.series[2].data.push();
+		chartData.series[2].data.push(countLocalOutliner(item.LOCAL_OUTLIER_INDICATOR, item.TARGET_VAR));
 		//chartData.series[3].data.push();
 		//chartData.series[4].data.push(countRange(countTrend(item.INTERCEPT, item.SLOPE), item.LOCAL_STANDARD_ERROR));
 		//chartData.series[4].type = 'arearange';
@@ -31,13 +31,18 @@ export function parseChartData(rdata) {
 	return chartData;
 }
 
-function countTrend(val, slope) {	
-	if(!slope) { return 0; }
+function countTrend(val: number, slope: number) {	
+	if(!slope) { return null; }
 	return val == 1 ? val + slope : val - 1 + slope;
 }
-function countRange(trend, loc_st_err) {
-	if(!loc_st_err) { return [0,0]; }
+function countRange(trend: number, loc_st_err: number) {
+	if(!loc_st_err) { return [null, null]; }
 	let low = trend - 1.5 * loc_st_err;
 	let high = trend + 1.5 * loc_st_err;
 	return [low, high];
+}
+
+function countLocalOutliner(loc_out_ind: number, val: number) {
+	if(!loc_out_ind || loc_out_ind == 0) { return null; }
+	return val;
 }
