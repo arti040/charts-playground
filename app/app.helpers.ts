9f5432d8def38dc4e05668876712cda88ec0f647
ppotera <ja@piotrpotera.com>
2017-12-05 12:23:57 +0100

    Next one serie added to the plot.

--- app/app.helpers.ts
+++ app/app.helpers.ts
@@ -6,7 +6,7 @@ export function parseChartData(rdata) {
 	let valShr = { color: '#06c', data: [], name: 'Val Shr' }	
 	let trend = { color: '#036', data: [], name: 'Trend' }
 	let localOutliner = { color: '#ffa500', data: [], type: 'scatter', dataLabels: { enabled: true, format: '{ point.y:.2f }' }, name: 'Local Outliner' }
-	let globalOutliner = { color: '#f00', data: [], name: 'Global Outliner' }
+	let globalOutliner = { color: '#f00', data: [], type: 'scatter', dataLabels: { enabled: true, format: '{ point.y:.2f }' }, name: 'Global Outliner' }
 	let range = { color: '#9DC3E3', data: [], name: 'Range' }
 	
 	chartData.marginRight = 220;
@@ -21,7 +21,7 @@ export function parseChartData(rdata) {
 		chartData.series[0].data.push(item.TARGET_VAR);
 		chartData.series[1].data.push(countTrend(item.INTERCEPT, item.SLOPE));
 		chartData.series[2].data.push(countLocalOutliner(item.LOCAL_OUTLIER_INDICATOR, item.TARGET_VAR));
-		//chartData.series[3].data.push();
+		chartData.series[3].data.push(countLocalOutliner(item.OUTLIER_INDICATOR, item.TARGET_VAR));
 		//chartData.series[4].data.push(countRange(countTrend(item.INTERCEPT, item.SLOPE), item.LOCAL_STANDARD_ERROR));
 		//chartData.series[4].type = 'arearange';
 		//chartData.series[4].fillOpacity = 0.3;
@@ -44,5 +44,10 @@ function countRange(trend: number, loc_st_err: number) {
 
 function countLocalOutliner(loc_out_ind: number, val: number) {
 	if(!loc_out_ind || loc_out_ind == 0) { return null; }
+	return val;
+}
+
+function countOutlinerIndicator(out_ind, val) {
+	if(!out_ind || out_ind == 0) { return null; }
 	return val;
 }
