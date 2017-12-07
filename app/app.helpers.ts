23e5d1091f81b12f19b95ac08a7b814b063e2555
Piotr Potera <piotr.potera@hpe.com>
2017-12-07 16:27:18 +0100

    Added periods.

--- app/app.helpers.ts
+++ app/app.helpers.ts
@@ -3,8 +3,11 @@ import { ChartData, chartDataModel } from './models/chartData.model';
 export function parseChartData(rdata) {
 	let chartData: chartDataModel = new ChartData();
 
-	let chartOpts= {
-		marginRight: 220,
+	let chartOpts = {
+		chart: { 		
+			width: 900,
+			marginRight: 220			
+		},
 		legend: {
 			align: 'right', 
 			verticalAlign: 'middle', 
@@ -22,7 +25,6 @@ export function parseChartData(rdata) {
 	}
 
 	let breakOpts = {
-		//borderWidth: 10,
 		color: null,
 		label: {
 			text: 'BREAK', 
@@ -42,11 +44,14 @@ export function parseChartData(rdata) {
 		zIndex: -1
 	}
 
-	let periodsOpts = {
-		text: '',
-		rotation: 90,
-		textAlign: 'left',
-		color: '#cce6ff',
+	let periodOpts = {
+		label: {
+			text: null,
+			rotation: 90,
+			textAlign: 'left',
+			color: '#cce6ff',
+			x: -5
+		},
 		from: null,
 		to: null
 	}
@@ -54,10 +59,17 @@ export function parseChartData(rdata) {
 	var trend: number = null;
 	var breaks: Array<any> = [];
 
-	chartData.marginRight = chartOpts.marginRight;
-	chartData.legend = chartOpts.legend;
+	chartData.xAxis.categories = [];
+
+	chartData = Object.assign({}, chartData, chartOpts);
+	console.log(chartData);
+
+	//chartData.marginRight = chartOpts.marginRight;
+	//chartData.legend = chartOpts.legend;
 
 	chartData.series.push(seriesOpts.valShr, seriesOpts.trend, seriesOpts.localOutliner, seriesOpts.globalOutliner, seriesOpts.range);
+	chartData.series[4] = Object.assign({}, chartData.series[4], rangeOpts);
+
 
 	rdata.forEach((item, idx, rdata) => {
 		let oldTrend = trend;	
@@ -65,7 +77,6 @@ export function parseChartData(rdata) {
 		trend = countTrend(rdata, oldTrend, idx, item.SLOPE, item.INTERCEPT);		
 
 		chartData.title.text = [item.MARKET,item.BRAND,item.MEASURE].join(' ');
-		chartData.xAxis.categories = [];
 		chartData.xAxis.categories.push(item.DATE);
 		
 		chartData.series[0].data.push(item.TARGET_VAR);
@@ -75,6 +86,8 @@ export function parseChartData(rdata) {
 		chartData.series[4].data.push(countRange(trend, item.LOCAL_STANDARD_ERROR));
 		
 		breaks.push(addPlotBreaks(breakOpts, idx, item.BREAKPOINT_INDICATOR, item.BREAKPOINT_SHIFT_ABS));
+		breaks = breaks.concat(addPeriods(periodOpts, idx, item.BREAKPOINT_INDICATOR, item.EARLY_WARNING_INDICATOR));
+		
 	});
 	console.log(breaks.filter((e) => { return e; }));
 	chartData.xAxis.plotBands = breaks.filter((e) => { return e; });
@@ -115,8 +128,8 @@ function countOutlierIndicator(out_ind, val) {
 	return !out_ind || out_ind == 0 ? null : val;
 }
 
-function addPlotBreaks(breakOpts: any, idx: number, brk_ind: number, brk_shift_abs: number) {	
-		let copy = JSON.parse(JSON.stringify(breakOpts));
+function addPlotBreaks(opts: any, idx: number, brk_ind: number, brk_shift_abs: number) {	
+		let copy = JSON.parse(JSON.stringify(opts));
 		if(brk_ind === 1) {		
 			copy.from = idx-1;
 			copy.to = idx;
@@ -126,23 +139,34 @@ function addPlotBreaks(breakOpts: any, idx: number, brk_ind: number, brk_shift_a
 		return null;
 }
 
-function addPeriodToAnal(opts: any, idx: number, early_wrn_ind: number, before: boolean) {
-	if(early_wrn_ind !== 0) {		
-		if(before) {
-			console.log(true);
-			opts.text = '3 Months before';
-			opts.from = idx - 3;
-			opts.to = idx - 1;
-		}
-		else {
-			console.log(false);
-			opts.text = '3 Months after';
-			opts.from = idx;
-			opts.to = idx + 2;
-		}
-		return opts;		
+function addPeriods(opts: any, idx: number, brk_ind: number, early_warn_ind: number) {
+	let before, after, periods = [];
+	before = JSON.parse(JSON.stringify(opts));
+	after = JSON.parse(JSON.stringify(opts));
+	before.label.text = '3 Months before';
+	after.label.text = '3 Months after';
+	before.color = '#cce6ff';
+	after.color = '#cce6ff';
+	
+	if(brk_ind !== 0) {		
+		before.from = idx - 3;
+		before.to = idx - 1;
+		after.from = idx;
+		after.to = idx + 2;
+
+		periods.push(before, after);		
 	}	
-	else { return null };
+
+	if(early_warn_ind !== 0) {		
+		before.from = idx - 3;
+		before.to = idx - 1;
+		after.from = idx;
+		after.to = idx + 2;
+
+		periods.push(before, after);	
+	}
+
+	return periods.length ? periods : null;
 }
 
 function addSlopeAddnotation() {
@@ -150,6 +174,3 @@ function addSlopeAddnotation() {
 	// liczę do góry
 	// w połowie wstawia label -> API
 }
-
-		// breaks[idx+1] = Object.assign(periodsOpts, { text: '3 Months before', from: idx-3, to: idx-1 });
-			// breaks[idx+2] = Object.assign(periodsOpts, { text: '3 Months after', from: idx, to: idx+2 });
