ff4676a40c8f6126585aefe30374793557ce30a4
Piotr Potera <ppotera@inovatica.com>
2017-12-31 13:55:44 +0100

    Hack for groupped columns added.

--- app/components/table/table.component.ts
+++ app/components/table/table.component.ts
@@ -35,4 +35,6 @@ export class DatatableComponent {
 // bgColor, color - sets colors for each ngx` table cell (as atomic class - see colors.atom)
 // action - defines action for each ngx' table cell
 // marked - adds a red traingle via CSS class to marked ngx' table cell 
+// icon - icon as a google-material-icons CSS class
+// iconSize - size of an icon (px) or default one from app.config file
 // (they sometimes need it - ask Tomek Zdunek for details)
